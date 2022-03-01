b5fc5c34d739e0477147207e43ebf39f2e906c45
trusteegain <trusteegain@gmail.com>
2022-03-01 18:36:34 +0100

    added niew

---
+++ main.js
@@ -0,0 +1,9 @@
+
+/* toggle menubar*/
+
+ function showMenu(){
+     var prince = document.getElementById("navLinks").style.right = "0px";
+ }
+ function hideMenu(){
+     var prince = document.getElementById("navLinks").style.right ="-200px";
+ }
