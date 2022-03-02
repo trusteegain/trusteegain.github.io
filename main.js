984c48acaea7a2d9daa900dbc4224b7a95b0626a
trusteegain <trusteegain@gmail.com>
2022-03-02 14:17:17 +0100

    new updates

--- main.js
+++ main.js
@@ -2,8 +2,13 @@
 /* toggle menubar*/
 
  function showMenu(){
-     var prince = document.getElementById("navLinks").style.width = "50%";
+     var prince = document.getElementById("navLinks").style.right = "0px";
  }
  function hideMenu(){
-     var prince = document.getElementById("navLinks").style.width ="0%";
+     var prince = document.getElementById("navLinks").style.right ="-200px";
+ }
+
+ function textBox(){
+    alert("we are so sorry at this moment we can't diplay any news");
+    alert("you can navigate around the website");
  }
