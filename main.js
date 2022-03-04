3487f17c5c5003eedc4094198c43ae9238c5e680
trusteegain <trusteegain@gmail.com>
2022-03-04 09:50:16 +0100

    NEW UPDATE

--- main.js
+++ main.js
@@ -13,4 +13,78 @@
     alert("you can navigate around the website");
  }
 
-  
+ function newParagraph(){
+     var x = document.getElementById("course-ul");
+     if (x.style.display  === "none"){
+         x.style.display = "block";
+     }
+     else{
+        x.style.display = "none";
+     }
+ }
+ 
+ function newParagraph(){
+     var a =document.getElementById("course-ul").style.display = "block";
+ }
+ function hide(){
+    var a =document.getElementById("course-ul").style.display = "none";
+}
+
+
+function newParagraph1(){
+    var a =document.getElementById("course-ul1").style.display = "block";
+}
+function hide1(){
+   var a =document.getElementById("course-ul1").style.display = "none";
+}
+
+
+function newParagraph2(){
+    var a =document.getElementById("course-ul2").style.display = "block";
+}
+function hide2(){
+   var a =document.getElementById("course-ul2").style.display = "none";
+}
+
+function newParagraph3(){
+    var a =document.getElementById("course-ul3").style.display = "block";
+}
+function hide3(){
+   var a =document.getElementById("course-ul3").style.display = "none";
+}
+
+function newParagraph4(){
+    var a =document.getElementById("course-ul4").style.display = "block";
+}
+function hide4(){
+   var a =document.getElementById("course-ul4").style.display = "none";
+}
+
+function newParagraph5(){
+    var a =document.getElementById("course-ul5").style.display = "block";
+}
+function hide5(){
+   var a =document.getElementById("course-ul5").style.display = "none";
+}
+
+function newParagraph6(){
+    var a =document.getElementById("course-ul6").style.display = "block";
+}
+function hide6(){
+   var a =document.getElementById("course-ul6").style.display = "none";
+}
+
+function newParagraph7(){
+    var a =document.getElementById("course-ul7").style.display = "block";
+}
+function hide7(){
+   var a =document.getElementById("course-ul7").style.display = "none";
+}
+
+
+function newParagraph8(){
+    var a =document.getElementById("course-ul8").style.display = "block";
+}
+function hide8(){
+   var a =document.getElementById("course-ul8").style.display = "none";
+}
